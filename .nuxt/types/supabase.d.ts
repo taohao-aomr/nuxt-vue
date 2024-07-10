77bd2e79e9bf67130cd964badcfc0b7c04943561
hao.tao <hao.tao@ttpai.cn>
2024-07-10 21:02:05 +0800

    feat: 修改go-page

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/Users/taohao/Vite/nuxt-vue/node_modules/.pnpm/@nuxtjs+supabase@1.2.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/Users/taohao/Vite/nuxt-vue/node_modules/.pnpm/@nuxtjs+supabase@1.2.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/Users/taohao/Vite/nuxt-vue/node_modules/.pnpm/@nuxtjs+supabase@1.2.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('/Users/taohao/Vite/nuxt-vue/node_modules/.pnpm/@nuxtjs+supabase@1.2.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
